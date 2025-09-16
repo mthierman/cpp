b2113ef2ab2d099c7cb69b75c381417686853d78
mthierman <mthierman@gmail.com>
2025-09-15 20:40:59 -0600

    Print some JSON

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,7 +1,17 @@
 #include <print>
+#include <string>
+#include <glaze/glaze.hpp>
+
+struct my_struct {
+    int i { 23 };
+    double d { 3.14 };
+    std::string hello { "hello" };
+};
 
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
-    std::println("TEST");
+    my_struct my_struct;
+    auto buffer { glz::write_json(my_struct).value_or("error") };
+    std::println("{}", buffer);
 
     return 0;
 }
