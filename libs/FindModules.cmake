08036d416d6071e0fdddde08c9248e9498d2338a
mthierman <mthierman@gmail.com>
2025-09-15 22:12:09 -0600

    Remove FindModules

--- libs/FindModules.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    cmake-modules
-    GIT_REPOSITORY "https://github.com/mthierman/cmake-modules.git"
-    GIT_TAG "main"
-)
-
-FetchContent_MakeAvailable(cmake-modules)
