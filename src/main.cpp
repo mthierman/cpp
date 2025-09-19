fc5200cdaac3f52c1eee4222b66b5a3ad5e8c024
mthierman <mthierman@gmail.com>
2025-09-18 21:41:38 -0600

    CLI11 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include "app.hpp"
 #include <CLI/CLI.hpp>
 
-auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int { return app(); }
+auto wmain(int argc, wchar_t* argv[], wchar_t* /* envp */[]) -> int {
+    CLI::App app { "App description" };
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    // CLI11_PARSE(app, argc, argv);
+    try {
+        (app).parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+        return (app).exit(e);
+    }
+    return 0;
+}
