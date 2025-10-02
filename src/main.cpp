b36e28a748fa4e38dcc0a49931b75d917c7df63a
mthierman <mthierman@gmail.com>
2025-10-01 18:42:30 -0600

    Add configure_file for version

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 // #include "app.hpp"
 #include <argz/argz.hpp>
 #include <pane/pane.hpp>
+#include <ranges>
 
 // auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int { return app(); }
 
-auto wmain(int argc, wchar_t* argv[], wchar_t* /* envp */[]) -> int {
+auto wmain(int /* argc */, wchar_t* /* argv[] */, wchar_t* /* envp */[]) -> int {
     auto args { pane::system::command_line_arguments() };
 
-    std::vector<const char*> argv_ptrs;
-    argv_ptrs.reserve(argc);
+    // First method:
+    //
+    // std::vector<const char*> argv_ptrs;
+    // argv_ptrs.reserve(argc);
 
-    for (auto& s : args) {
-        argv_ptrs.push_back(reinterpret_cast<const char*>(s.c_str()));
-    }
+    // for (auto& s : args) {
+    //     argv_ptrs.push_back(reinterpret_cast<const char*>(s.c_str()));
+    // }
+
+    // Second method:
+    auto argv_ptrs = std::ranges::to<std::vector<const char*>>(
+        args | std::views::transform([](const std::u8string& s) {
+        return reinterpret_cast<const char*>(s.c_str());
+    }));
 
     constexpr std::string_view version = "1.2.3";
     argz::about about { "My program description", version };
@@ -29,7 +38,7 @@ auto wmain(int argc, wchar_t* argv[], wchar_t* /* envp */[]) -> int {
                          { { "number_opt" }, number_opt, "input an int" } };
 
     try {
-        argz::parse(about, opts, argc, argv_ptrs.data());
+        argz::parse(about, opts, argv_ptrs.size(), argv_ptrs.data());
     } catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
     }
