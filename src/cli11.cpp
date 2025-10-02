c63665329e740ca4dc94116efe664cdc2e0c4762
mthierman <mthierman@gmail.com>
2025-10-02 15:45:03 -0600

    Version

--- src/cli11.cpp
+++ src/cli11.cpp
@@ -3,14 +3,16 @@
 
 auto wmain(int argc, wchar_t* argv[], wchar_t* /* envp */[]) -> int {
     CLI::App app { "App description" };
+    app.set_version_flag("--version", std::string(CLI11_VERSION));
 
     std::string filename = "default";
     app.add_option("-f,--file", filename, "A help string");
 
     try {
-        (app).parse(argc, argv);
+        app.parse(argc, argv);
     } catch (const CLI::ParseError& e) {
-        return (app).exit(e);
+        return app.exit(e);
     }
+
     return 0;
 }
