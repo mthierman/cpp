9ba9baf326744bb18e4253dfd404dfccf3179031
mthierman <mthierman@gmail.com>
2025-10-01 22:55:28 -0600

    Config

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,18 @@ auto wmain(int /* argc */, wchar_t* /* argv[] */, wchar_t* /* envp */[]) -> int 
     argz::about about { reinterpret_cast<const char*>(PROJECT_DESCRIPTION.data()),
                         reinterpret_cast<const char*>(PROJECT_VERSION.data()) };
 
-    std::string input {};
-    std::string study {};
-    int number = 123;
-    bool boolean = true;
-    std::optional<int> number_opt {};
-    argz::options opts { { { "input", 'i' }, input, "the input file" },
-                         { { "study", 's' }, study, "a study file" },
-                         { { "number" }, number, "input an int" },
-                         { { "boolean" }, boolean, "a boolean" },
-                         { { "number_opt" }, number_opt, "input an int" } };
+    std::optional<std::string> config { std::nullopt };
+
+    argz::options opts { { { "config", 'c' }, config, "config file" } };
 
     try {
         argz::parse(about, opts, argv.size(), argv.data());
     } catch (const std::exception& e) {
         std::println("{}", e.what());
+    }
+
+    if (config) {
+        std::println("{}", *config);
     }
 
     return 0;
