0c7a441686093c67159d8823238d9a96df21de34
mthierman <mthierman@gmail.com>
2025-10-02 15:32:39 -0600

    Add cli11

---
+++ src/cli11.cpp
@@ -0,0 +1,17 @@
+#include <string>
+// #include <cli
+
+auto wmain(int argc, wchar_t* argv[], wchar_t* /* envp */[]) -> int {
+    CLI::App app { "App description" };
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    // CLI11_PARSE(app, argc, argv);
+    try {
+        (app).parse(argc, argv);
+    } catch (const CLI::ParseError& e) {
+        return (app).exit(e);
+    }
+    return 0;
+}
