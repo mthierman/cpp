30fc9b95358a92d4495ca7825d4999cb80317fe1
mthierman <mthierman@gmail.com>
2025-10-02 20:21:17 -0600

    Use u8string for colors

--- src/color.hpp
+++ src/color.hpp
@@ -22,75 +22,76 @@ enum class color {
     bright_white
 };
 
-inline constexpr const char* get_color(color c) {
+inline constexpr const char8_t* get_color(color c) {
     switch (c) {
         case color::none:
-            return "\x1b[0m";
+            return u8"\x1b[0m";
         case color::black:
-            return "\x1b[30m";
+            return u8"\x1b[30m";
         case color::red:
-            return "\x1b[31m";
+            return u8"\x1b[31m";
         case color::green:
-            return "\x1b[32m";
+            return u8"\x1b[32m";
         case color::yellow:
-            return "\x1b[33m";
+            return u8"\x1b[33m";
         case color::blue:
-            return "\x1b[34m";
+            return u8"\x1b[34m";
         case color::magenta:
-            return "\x1b[35m";
+            return u8"\x1b[35m";
         case color::cyan:
-            return "\x1b[36m";
+            return u8"\x1b[36m";
         case color::white:
-            return "\x1b[37m";
+            return u8"\x1b[37m";
         case color::bright_black:
-            return "\x1b[90m";
+            return u8"\x1b[90m";
         case color::bright_red:
-            return "\x1b[91m";
+            return u8"\x1b[91m";
         case color::bright_green:
-            return "\x1b[92m";
+            return u8"\x1b[92m";
         case color::bright_yellow:
-            return "\x1b[93m";
+            return u8"\x1b[93m";
         case color::bright_blue:
-            return "\x1b[94m";
+            return u8"\x1b[94m";
         case color::bright_magenta:
-            return "\x1b[95m";
+            return u8"\x1b[95m";
         case color::bright_cyan:
-            return "\x1b[96m";
+            return u8"\x1b[96m";
         case color::bright_white:
-            return "\x1b[97m";
+            return u8"\x1b[97m";
     }
 }
 
 enum class style { none, bold, dim, italic, underline, inverse, hidden };
 
-inline constexpr const char* get_style(style s) {
+inline constexpr const char8_t* get_style(style s) {
     switch (s) {
         case style::none:
-            return "\x1b[0m";
+            return u8"\x1b[0m";
         case style::bold:
-            return "\x1b[1m";
+            return u8"\x1b[1m";
         case style::dim:
-            return "\x1b[2m";
+            return u8"\x1b[2m";
         case style::italic:
-            return "\x1b[3m";
+            return u8"\x1b[3m";
         case style::underline:
-            return "\x1b[4m";
+            return u8"\x1b[4m";
         case style::inverse:
-            return "\x1b[7m";
+            return u8"\x1b[7m";
         case style::hidden:
-            return "\x1b[8m";
+            return u8"\x1b[8m";
     }
 }
 
-template <color c = color::none, style... styles> inline std::string paint(std::string_view text) {
-    std::string result;
+template <color c = color::none, style... styles>
+inline std::u8string paint(std::u8string_view text) {
+    std::u8string result;
     ((result += get_style(styles)), ...);
     result += get_color(c);
-    result += std::string { text } + get_style(style::none);
+    result += std::u8string { text } + get_style(style::none);
 
     return result;
 }
 
-template <style... styles> inline auto paint(std::string_view text) -> std::string {
+template <style... styles> inline auto paint(std::u8string_view text) -> std::u8string {
     return paint<color::none, styles...>(text);
 }
