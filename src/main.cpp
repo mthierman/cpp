2e47bcc1ae6ad1a8488657455f3f70c87d2528ea
mthierman <mthierman@gmail.com>
2025-10-03 12:21:26 -0600

    Consolidate args printing

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,10 @@ using namespace std::string_view_literals;
 
 auto wmain(int /* argc */, wchar_t* /* argv[] */, wchar_t* /* envp */[]) -> int {
     auto argv { pane::system::command_line_arguments() };
-    std::println("argv.size(): {}", argv.size());
     std::println("clig.dev: {}", u8"https://clig.dev/");
 
     for (auto [idx, arg] : std::views::enumerate(argv) | std::views::as_const) {
-        std::println("{}: {}", idx, arg);
+        std::println("{}/{}: {}", idx, argv.size(), arg);
     }
 
     std::println();
