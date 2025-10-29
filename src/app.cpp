9c321478249e91a038e9faa3dc58c56c07b04902
mthierman <mthierman@gmail.com>
2025-10-28 23:06:39 -0600

    Fix sources

--- src/app.cpp
+++ src/app.cpp
@@ -1,19 +1,27 @@
 #include "app.hpp"
-#include <print>
+#include "color.hpp"
 #include <pane/pane.hpp>
+#include <print>
 
-auto cpp::print() -> void {
-    std::u8string buffer;
+namespace app {
+auto run() -> int {
+    auto argv { pane::system::get_argv_u8() };
 
-    if (auto ec { glz::write_json(this, buffer) }; !ec) {
-        std::println("{}", buffer);
+    std::println("{}", u8"https://clig.dev/");
+
+    for (auto [idx, arg] : std::views::enumerate(argv) | std::views::as_const) {
+        std::println("{}/{}: {}", idx, argv.size(), arg);
     }
-}
 
-auto app() -> int {
-    cpp cpp;
+    std::println();
 
-    cpp.print();
+    if (argv.size() == 1) {
+        std::println();
+        std::print("{}", paint<color::bright_blue, style::bold>(u8"Usage: "));
+        std::println("{}", paint<color::bright_cyan>(u8"cv.exe [OPTIONS] <COMMAND>"));
+        std::println();
+    }
 
     return 0;
 }
+} // namespace app
--- src/app.hpp
+++ src/app.hpp
@@ -1,14 +1,5 @@
 #pragma once
 
-#include <string>
-
-struct cpp {
-    int i { 22 };
-    double d { 3.14 };
-    std::string hello { "hello" };
-    std::string test { "test" };
-
-    auto print() -> void;
-};
-
-auto app() -> int;
+namespace app {
+auto run() -> int;
+} // namespace app
